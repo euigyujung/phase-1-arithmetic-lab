86dde2c46d69867e1e9c2578761566c4ccb9069d
euigyu <euigyu.jung@gmail.com>
2021-10-10 15:26:59 -0700

    commit tree cefb6ac3d5ce7baded38e5c9b7a6792e98a34a6c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+let random =Math.floor(Math.random()*10)
+
+
+
+let num3 = 16
+let num4 = 12
+const mod = num3 % num4
+
+let max = Math.max(1,5,20)
+
 
 // Write your code here
